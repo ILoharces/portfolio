04e4d2dc46fe575b5b320b37ce4db3ac1fd45d4f
Ibai <ibai.loharces@gmail.com>
2025-05-28 18:02:40 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Hola mundo con React + TypeScript + Vite</h1>
+    </div>
+  );
+}
+
+export default App;
